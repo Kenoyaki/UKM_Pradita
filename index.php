da6d99e9c8fb24441aa61e9f5f405cd8f7466c6d
JulianYang345 <julianyang345@gmail.com>
2023-11-07 14:19:16 +0700

    Mengecilkan padding x navbar

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-            <div class="container px-4 px-lg-5">
+            <div class="container px-4 px-lg-1">
             <img class="img-fluid" src="assets/img/logopradita.png" alt="..." width = 10% />
                 <a class="navbar-brand" href="#page-top" style = "padding-left : 1%"></a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
